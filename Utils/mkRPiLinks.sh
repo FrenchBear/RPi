39e2b6e59b2eda9e9eebca81116d1181d9b71cd4
FrencgBear@gluon2 <FrenchBear38@outlook.com>
2018-03-29 01:08:42 +0200

    merge gluon2

--- Utils/mkRPiLinks.sh
+++ Utils/mkRPiLinks.sh
@@ -6,8 +6,18 @@
 for dir in Asm BlueJ C FrameBuffer Fuze Greenfoot Java Mathematica Node.js Ruby Scratch SDL2a SDL2b SonicPi Utils Zmq; do
 	if ! test -L ~/$dir
 	then
-	  echo "Create link for $dir"
-	  ln -s ~/Development/GitHub/RPi/$dir ~/$dir
+		echo "Create link for $dir"
+		ln -s ~/Development/GitHub/RPi/$dir ~/$dir
 	fi
 done
 
+if test -d ~/Development/GitHub/RPiMedia; then
+	echo Media
+	for dir in Documents Music Pictures Videos; do
+		if ! test -L ~/$dir
+		then
+			echo "Create link for $dir"
+			ln -s ~/Development/GitHub/RPiMedia/$dir ~/$dir
+		fi
+	done
+fi
