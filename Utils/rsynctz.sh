4b0909fbaa5709d3e51ab990a1c88459f33f333b
FrenchBear (gluon3/pihole) <FrenchBear38@outlook.com>
2022-08-28 10:57:59 +0200

    fixed rsynctz.sh

--- Utils/rsynctz.sh
+++ Utils/rsynctz.sh
@@ -7,7 +7,7 @@
 cd ~
 sudo mount //teraz/backup_rpi /mnt -o user=tera -o pass='Astérix!' -o vers=1.0 && {
 	root=/mnt/`uname -n`
-	root=/mnt/gluon4b4-2
+#	root=/mnt/gluon4b4-2
 	[ -d $root ] || mkdir $root
 	sudo rsync -rtvH --delete --exclude='.cache/*' --exclude='.local/share/Trash/*' --exclude='GitHub/*' --exclude='GitVSTS/*' . $root/
 	sudo umount /mnt
